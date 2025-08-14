b2f94993f4aa2b48cf21e3db9aa2bad2a74da095
tmdgusya <dev0jsh@gmail.com>
2025-08-15 01:01:43 +0900

    string

--- string_test/src/main.rs
+++ string_test/src/main.rs
@@ -1,6 +1,17 @@
 fn main() {
+    let s = String::from("hello");
+    let len = calculate_length(&s);
+    println!("The length of '{}' is {}.", s, len);
+
     let mut s = String::from("hello");
+    change(&mut s);
     println!("{}", s);
-    s.push_str(" world");
-    println!("{}", s);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
